463698fcaa3e2124acfec45a7a6586fb1b0ee929
Christina Elias <eliaschristina4@gmail.com>
2024-01-26 20:43:21 -0500

    fixing fetch endpoint

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -15,7 +15,7 @@ export default function Shop () {
     }, []);
 
     async function fetchInventory() {
-        const endpoint = '/db';
+        const endpoint = 'https://collective-candle.onrender.com/db';
         const response = await fetch(endpoint);
        // console.log(response);
 
